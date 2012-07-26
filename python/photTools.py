8e8304443038c8759106b3be6a097fdf1b9fa1d9
rhiannonlynne <rhiannon.lynne@gmail.com>
2012-07-26 16:06:27 -0700

    Added LSST licensing info, added some clarity to Bandpass regarding wavelength limits.
    Moved wavelen_min/_max/_step into internal variables to Bandpass, to
    make it easier to use nondefault values.

--- python/photTools.py
+++ python/photTools.py
@@ -1,3 +1,25 @@
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010, 2011, 2012 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
 import os
 from copy import deepcopy
 import numpy
@@ -15,13 +37,13 @@ from Bandpass import Bandpass
 figformat = 'png'
 
 EXPTIME = 15                      # Default exposure time. (option for method calls).                   
-NEXP = 2                          # Default number of exposures. (option for methods).                                                  
-EFFAREA = numpy.pi*(6.5*100/2.0)**2   # Default effective area of primary mirror. (option for methods).                                
-GAIN = 2.3                        # Default gain. (option for method call).                                                            
-RDNOISE = 5                       # Default value - readnoise electrons or adu per pixel (per exposure)                                 
-DARKCURRENT = 0.2                 # Default value - dark current electrons or adu per pixel per second                                  
-OTHERNOISE = 4.69                 # Default value - other noise electrons or adu per pixel per exposure                                 
-PLATESCALE = 0.2                  # Default value - "/pixel                                                                             
+NEXP = 2                          # Default number of exposures. (option for methods).
+EFFAREA = numpy.pi*(6.5*100/2.0)**2   # Default effective area of primary mirror. (option for methods).
+GAIN = 2.3                        # Default gain. (option for method call).
+RDNOISE = 5                       # Default value - readnoise electrons or adu per pixel (per exposure)
+DARKCURRENT = 0.2                 # Default value - dark current electrons or adu per pixel per second
+OTHERNOISE = 4.69                 # Default value - other noise electrons or adu per pixel per exposure
+PLATESCALE = 0.2                  # Default value - "/pixel
 SEEING = {'u': 0.77, 'g':0.73, 'r':0.70, 'i':0.67, 'z':0.65, 'y':0.63}  # Default seeing values (in ")     
 
 
@@ -213,7 +235,7 @@ def makeRedshiftedSeds(sedDict, sedlists, redshifts):
                 # And add new SEDs redshifted to this z for this type of object. 
                 for s in sedlists[objtype]:
                     # Make the new name for the new SED at this redshift. 
-                    newsedname = s + '_Z%.3f' %(z)
+                    newsedname = s + '_Z_%.3f' %(z)
                     # Add it to the SED dictionary. 
                     sedDict[newsedname] = redshiftSingleSED(sedDict[s], z)
                     newSedsForList.append(newsedname)
@@ -228,7 +250,7 @@ def makeRedshiftedSeds(sedDict, sedlists, redshifts):
                     # Skip stars.
                     continue
                 for s in sedlists[objtype]:
-                    newsedname = s + '_%.3f' %(z)
+                    newsedname = s + '_Z_%.3f' %(z)
                     sedDicts[newsedname] = redshiftSingleSED(sedDict[s], z)
                     newSedsForList.append(newsedname)
                 sedlists[objtype] += newSedsForList
@@ -238,8 +260,28 @@ def makeRedshiftedSeds(sedDict, sedlists, redshifts):
 def redshiftSingleSED(sed_in, z):
     # Make a copy of the input SED, as otherwise we will overwrite original arrays.
     sed_out = deepcopy(sed_in)
-    sed_out.redshiftSED(z)
+    sed_out.redshiftSED(z, dimming=True)
     return sed_out
+
+
+def matchSedsBp(sedDict, bpDict, refFilter=None):
+    """Match the wavelength ranges for all the Seds and the bandpass dictionary.
+    This will speed up later calculation of magnitudes (if you're doing a lot of them),
+     but is not strictly necessary. """
+    if refFilter == None:
+        refFilter = bpDict.keys()[0]
+    wavelen_match = bpDict[refFilter].wavelen
+    # Check all filters in bpDict match in wavelength space (note bandpasses must be regular grid).
+    for f in bpDict.keys():
+        if numpy.any(bpDict[f].wavelen != wavelen_match):
+            bpDict[f].resampleBandpass(wavelen_min=wavelen_match.min(), wavelen_max=wavelen_match.max(),
+                                       wavelen_step = wavelen_match[1] - wavelen_match[0])            
+    # Check all seds in sedDict match in wavelength space. 
+    for s in sedDict.keys():    
+        if sedDict[s].needResample(wavelen_match=wavelen_match):
+            sedDict[s].resampleSED(wavelen_match=wavelen_match)
+    return sedDict, bpDict
+
 
 def calcNatMags(bandpassDict, sedDict, sedlists):
     """Calculate (zeropoint-calibrated) natural magnitudes for each SED,
